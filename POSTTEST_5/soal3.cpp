dbf5073b79a559ea19e8c882ca63c8918fdc65d3
kelvin alviansyah <104289852+kelvinaavas@users.noreply.github.com>
2025-10-15 00:06:54 +0800

    POSTTEST_5

---
+++ POSTTEST_5/soal3.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+Node* insert(Node* root, int val) {
+    if (root == nullptr) {
+        return new Node(val);
+    }
+    if (val < root->data) {
+        root->left = insert(root->left, val);
+    } else if (val > root->data) {
+        root->right = insert(root->right, val);
+    }
+    return root;
+}
+
+int findMaxValue(Node* root) {
+    if (root == nullptr) {
+        return -1;
+    }
+    while (root->right != nullptr) {
+        root = root->right; 
+    }
+    return root->data;
+}
+
+int main() {
+    Node* root = nullptr;
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 70);
+    insert(root, 20);
+    insert(root, 80);
+
+    cout << "Nilai terbesar dalam tree adalah: " << findMaxValue(root) << endl;
+
+    return 0;
+}
